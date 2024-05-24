ecf0004e08e262d8318908e4ffc54f7ec3cdb189
ANA CLARA PACHECO MILLER DA SILVA <ana.silva4957>
2024-05-24 11:36:34 -0300

    ajuste de layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,9 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <h1>Projeto</h1>
+    <div className="container">
+      <h1>Projeto</h1>
+    </div>
+    
   );
 }
